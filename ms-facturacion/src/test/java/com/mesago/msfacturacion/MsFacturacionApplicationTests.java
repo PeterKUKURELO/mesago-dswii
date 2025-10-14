506c51c715da2e6d5a49531ba2a117b23a13396f
Peter Kukurelo <gabrielkukurelo@gmail.com>
2025-10-13 23:35:25 -0500

    Proyecto base MesaGO - estructura de microservicios

---
+++ ms-facturacion/src/test/java/com/mesago/msfacturacion/MsFacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesago.msfacturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFacturacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
