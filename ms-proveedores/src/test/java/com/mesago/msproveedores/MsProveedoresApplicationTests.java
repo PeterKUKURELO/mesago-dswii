823673f19d2173af1c695a994c33ec1640400024
Peter Kukurelo <gabrielkukurelo@gmail.com>
2025-10-18 04:08:13 -0500

    Actualización properties mesa-go

---
+++ ms-proveedores/src/test/java/com/mesago/msproveedores/MsProveedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesago.msproveedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProveedoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
