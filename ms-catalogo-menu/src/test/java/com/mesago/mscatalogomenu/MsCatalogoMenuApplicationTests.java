2f2136e2d7cd284f8f30957d237c85b55d446101
Peter Kukurelo <gabrielkukurelo@gmail.com>
2025-10-18 04:52:15 -0500

    ultimo cambio en ms

---
+++ ms-catalogo-menu/src/test/java/com/mesago/mscatalogomenu/MsCatalogoMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesago.mscatalogomenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCatalogoMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
