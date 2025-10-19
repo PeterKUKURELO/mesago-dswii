d0631752a5eb932477ddbec1408d40622d2294ec
mStagnaro17 <michele.stagnaro@gmail.com>
2025-10-19 00:22:36 -0500

    qw wq On branch development

--- gateway-service/src/main/java/com/mesago/gatewayservice/config/jwt/GatewayMvcConfig.java
+++ gateway-service/src/main/java/com/mesago/gatewayservice/config/jwt/GatewayMvcConfig.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.HandlerFunction;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
@@ -13,14 +12,26 @@ public class GatewayMvcConfig {
 
     @Bean
     public RouterFunction<ServerResponse> gatewayRouter() {
-        return GatewayRouterFunctions
-                .route("ms-auth-module")
-                .route(request ->
-                        request.path().startsWith("/api/auth") ||
-                        request.path().startsWith("/api/users") ||
-                        request.path().startsWith("/api/admin"),
+
+        return GatewayRouterFunctions.route()
+                // Autenticación
+                .route(request -> request.path().startsWith("/api/auth")
+                                || request.path().startsWith("/api/users")
+                                || request.path().startsWith("/api/admin"),
                         HandlerFunctions.http("http://localhost:8081"))
+
+                // Pedidos
+                .route(request -> request.path().startsWith("/api/pedidos"),
+                        HandlerFunctions.http("http://localhost:8082"))
+
+                // Inventario
+                .route(request -> request.path().startsWith("/api/insumos"),
+                        HandlerFunctions.http("http://localhost:8083"))
+
+                // Clientes
+                .route(request -> request.path().startsWith("/api/clientes"),
+                        HandlerFunctions.http("http://localhost:8084"))
+
                 .build();
     }
-
 }
