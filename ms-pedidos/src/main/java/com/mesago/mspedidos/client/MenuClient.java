41fef53682b293821207f5774f58672d2431e453
mStagnaro17 <michele.stagnaro@gmail.com>
2025-10-31 14:08:12 -0500

    Me encanta hacer cambios 1 hora antes de exponer

--- ms-pedidos/src/main/java/com/mesago/mspedidos/client/MenuClient.java
+++ ms-pedidos/src/main/java/com/mesago/mspedidos/client/MenuClient.java
@@ -1,5 +1,6 @@
 package com.mesago.mspedidos.client;
 
+import com.mesago.mspedidos.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.math.BigDecimal;
 import java.util.Map;
 
-@FeignClient(name = "ms-catalogo-menu", url = "http://ms-catalogo-menu:8088/api/menu")
+@FeignClient(
+        name = "ms-catalogo-menu",
+        url = "http://ms-catalogo-menu:8088/api/menu",
+        configuration = FeignClientConfig.class
+)
 public interface MenuClient {
-
     @GetMapping("/{id}")
     Map<String, Object> obtenerMenuPorId(@PathVariable("id") Long idMenu);
 }
